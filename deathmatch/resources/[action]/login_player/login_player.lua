05eba634f7c3193b37866af83417cdacf36dacca
fF <framef318@gmail.com>
2025-11-02 14:12:01 +0700

    Refactor loginPlayer function to improve command handling and success message output

--- deathmatch/resources/[action]/login_player/login_player.lua
+++ deathmatch/resources/[action]/login_player/login_player.lua
@@ -1,10 +1,11 @@
-local function loginPlayer(thePlayer, command, username, password)
+function loginPlayer(thePlayer, command, username, password)
     local account = getAccount(username, password)                         -- Return the account
     if (account ~= false) then                                             -- If the account exists.
         logIn(thePlayer, account, password) 
+        outputChatBox("You have successfully logged in, " .. username .. "!", thePlayer, 0, 255, 0) -- Output success message.
     else
         outputChatBox("Wrong username or password!", thePlayer, 255, 255, 0) -- Output they got the details wrong.
     end
 end
 
-addCommandHandler("login", loginPlayer)
+addCommandHandler("log-in", loginPlayer)
