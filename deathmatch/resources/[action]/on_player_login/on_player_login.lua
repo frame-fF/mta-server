bae6eed1349064d8b8aed04b8f094cb6cc416246
fF <framef318@gmail.com>
2025-11-03 16:59:45 +0700

    Refactor onPlayerLogin and get_player_account functions to streamline player account retrieval and improve code clarity

--- deathmatch/resources/[action]/on_player_login/on_player_login.lua
+++ deathmatch/resources/[action]/on_player_login/on_player_login.lua
@@ -1,14 +1,5 @@
 local function onPlayerLogin()
-    -- -- สปอนผู้เล่นที่ San Fierro
-    -- spawnPlayer(source, -1969.4, 137.85, 27.69, 0, 0, 0, 0)
-    -- fadeCamera(source, true)
-    -- setCameraTarget(source, source)
-
-    -- local account = getPlayerAccount(source)
-    -- local username = getAccountName(account)
-    
-    -- result = exports.api_player:get_player_account(source, username)
-
+    local result = exports.api_player:get_player_account(source)
     outputChatBox("Welcome to the server, " .. getPlayerName(source) .. "!")
 end
 
--- deathmatch/resources/[api]/api_player/get_player_account.lua
+++ deathmatch/resources/[api]/api_player/get_player_account.lua
@@ -1,58 +1,16 @@
-function get_player_account(source, username)
-    -- Get API URL from global server configuration
-    local url = get("api_base_url") or "http://127.0.0.1:8000"
-    local apiEndpoint = url .. '/userdata/' .. username
-    
-    sendOptions = {
+function get_player_account(source)
+    local username = getPlayerName(source)
+    local url = "http://127.0.0.1:8000" .. '/api/player/' .. username .. '/'
+    local sendOptions = {
         connectionAttempts = 3,
         connectTimeout = 5000,
         method = "GET",
-        formFields = {},
     }
-    fetchRemote(apiEndpoint, sendOptions, function(data, info)
-        if info.success then
-            outputChatBox('load data success', source, 0, 255, 0)
-            iprint('load data success')
-            local result = fromJSON(data)
-            local x, y, z = unpack(result.position[1])
-
-            team = result.team and getTeamFromName(result.team)
-
-            spawnPlayer (
-                source,
-                x, y, z, 
-                result.rotation, 
-                result.skin , 
-                result.interior, 
-                result.dimension, 
-                team
-            )
-
-            local weapons = unpack(result.weapons)
-            for weapon, ammo in pairs(weapons) do
-                giveWeapon(source, weapon, ammo)
-            end
-
-            setElementHealth(source, result.health)
-            setPedArmor(source, result.armor)
-            setPlayerMoney(source, result.money)
-            setPlayerWantedLevel(source, result.wantedlevel)
-            
-            clothes = unpack(result.clothes)
-
-            for _, cloth in ipairs(clothes) do
-                addPedClothes(source, cloth[1], cloth[2], cloth[3])
-            end
-
-            for _, stat in ipairs({ 69, 70, 71, 72, 73, 74, 76, 77, 78, 79 }) do
-                setPedStat(source, stat, 1000)
-                -- outputChatBox("Your game stats upgraded to maximum!", source, 0, 255, 0, false)
-             end
-            
-            fadeCamera (source, true)
-	        setCameraTarget (source, source)
+    fetchRemote(url, sendOptions, function(data, info)
+        if info.statusCode == 200 then
+            iprint(data)
         else
-            outputChatBox('Error: ' .. data, source, 255, 0, 0)
+            outputChatBox('Error: ' .. data, source, 255, 255, 0)
         end
     end)
 end
