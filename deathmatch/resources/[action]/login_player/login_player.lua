fa6c0762392104420a60c6586992e32ca08d443d
fF <framef318@gmail.com>
2025-11-03 14:11:53 +0700

    Refactor get_account and loginPlayer functions to improve code clarity and consistency

--- deathmatch/resources/[action]/login_player/login_player.lua
+++ deathmatch/resources/[action]/login_player/login_player.lua
@@ -1,6 +1,5 @@
 function loginPlayer(thePlayer, command, username, password)
     local result = exports.api_player:get_account(thePlayer, username, password)
-    
 end
 
 addCommandHandler("log-in", loginPlayer) 
--- deathmatch/resources/[api]/api_player/get_account.lua
+++ deathmatch/resources/[api]/api_player/get_account.lua
@@ -1,6 +1,5 @@
 function get_account(source, username, password)
     local url = "http://127.0.0.1:8000" .. '/api/player/login/'
-
     sendOptions = {
         connectionAttempts = 3,
         connectTimeout = 5000,
@@ -11,8 +10,8 @@ function get_account(source, username, password)
         }
     }
     fetchRemote(url, sendOptions, function(data, info)
-        iprint("----", data, info.statusCode)
-        if info.success then
+        iprint(data)
+        if info.statusCode == 200 then
             outputChatBox('login success', source, 0, 255, 0)
         else
             outputChatBox('Error: ' .. data, source, 255, 255, 0)
