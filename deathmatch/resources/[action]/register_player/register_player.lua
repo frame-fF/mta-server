200269d1bf4ddb1963daf8eb7d34fc513bcc0e1a
fF <framef318@gmail.com>
2025-11-04 00:04:29 +0700

    Update resource startup settings and fix command handler for registration

--- deathmatch/resources/[action]/register_player/register_player.lua
+++ deathmatch/resources/[action]/register_player/register_player.lua
@@ -2,7 +2,7 @@ function registerPlayer(source, commandName, username, email, password, password
     exports.api_player:register(source, username, email, password, password2)
 end
 
-addCommandHandler("register", registerPlayer)
+addCommandHandler("regis-ter", registerPlayer)
 
 function register_player(source, username, email, password, password2)
     exports.api_player:register(source, username, email, password, password2)
