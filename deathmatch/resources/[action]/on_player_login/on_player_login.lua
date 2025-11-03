e35ed07e770c8a0b81e599d9fd77602e32ef02f8
fF <framef318@gmail.com>
2025-11-03 10:54:10 +0700

    Implement API player account retrieval and update ACL configuration

--- deathmatch/resources/[action]/on_player_login/on_player_login.lua
+++ deathmatch/resources/[action]/on_player_login/on_player_login.lua
@@ -4,6 +4,11 @@ local function onPlayerLogin()
     spawnPlayer(source, -1969.4, 137.85, 27.69, 0, 0, 0, 0)
     fadeCamera(source, true)
     setCameraTarget(source, source)
+
+    local account = getPlayerAccount(source)
+    local username = getAccountName(account)
+    
+    result = exports.api_player:get_player_account(source, username)
 end
 
 addEventHandler("onPlayerLogin", root, onPlayerLogin)
---
+++ deathmatch/resources/[api]/api_player/get_player_account.lua
@@ -0,0 +1,55 @@
+function get_player_account(source, username)
+    local url =  get("url") .. '/userdata/' .. username
+    sendOptions = {
+        connectionAttempts = 3,
+        connectTimeout = 5000,
+        method = "GET",
+        formFields = {},
+    }
+    fetchRemote(url, sendOptions, function(data, info)
+        if info.success then
+            outputChatBox('load data success', source, 0, 255, 0)
+            iprint('load data success')
+            local result = fromJSON(data)
+            local x, y, z = unpack(result.position[1])
+
+            team = result.team and getTeamFromName(result.team)
+
+            spawnPlayer (
+                source,
+                x, y, z, 
+                result.rotation, 
+                result.skin , 
+                result.interior, 
+                result.dimension, 
+                team
+            )
+
+            local weapons = unpack(result.weapons)
+            for weapon, ammo in pairs(weapons) do
+                giveWeapon(source, weapon, ammo)
+            end
+
+            setElementHealth(source, result.health)
+            setPedArmor(source, result.armor)
+            setPlayerMoney(source, result.money)
+            setPlayerWantedLevel(source, result.wantedlevel)
+            
+            clothes = unpack(result.clothes)
+
+            for _, cloth in ipairs(clothes) do
+                addPedClothes(source, cloth[1], cloth[2], cloth[3])
+            end
+
+            for _, stat in ipairs({ 69, 70, 71, 72, 73, 74, 76, 77, 78, 79 }) do
+                setPedStat(source, stat, 1000)
+                -- outputChatBox("Your game stats upgraded to maximum!", source, 0, 255, 0, false)
+             end
+            
+            fadeCamera (source, true)
+	        setCameraTarget (source, source)
+        else
+            outputChatBox('Error: ' .. data, source, 255, 0, 0)
+        end
+    end)
+end
