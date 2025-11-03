d6cedbfd54bcc09d44b01f300d5a4dc6c2fedea8
fF <framef318@gmail.com>
2025-11-03 21:22:12 +0700

    Add on_player_quit resource and implement player data saving on quit

---
+++ deathmatch/resources/[action]/on_player_quit/on_player_quit.lua
@@ -0,0 +1,57 @@
+function onPlayerQuit()
+    local account = getPlayerAccount(source)
+    local username = getAccountName(account)
+
+    local x, y, z = getElementPosition(source)
+    local rotation = getPedRotation(source)
+    local skin = getElementModel(source)
+    local interior = getElementInterior(source)
+    local dimension = getElementDimension(source)
+    local team = getPlayerTeam(source)
+
+    local weapon = getPedWeapon(source)
+    local ammo = getPedTotalAmmo(source)
+    local health = getElementHealth(source)
+    local armor = getPedArmor(source)
+    local money = getPlayerMoney(source)
+    local wantedlevel = getPlayerWantedLevel(source)
+
+    local position = toJSON({x, y, z})
+    local team = team and getTeamName(team)
+    local weapons = { }
+    for slot=1, 12 do
+        local weapon = getPedWeapon(source, slot)
+        local ammo = getPedTotalAmmo(source, slot)
+        if (weapon > 0) and (ammo > 0) then
+            weapons[weapon] = ammo
+        end
+    end
+
+    local clothes = { }
+    for type=0, 17 do
+        local texture, model = getPedClothes(source, type)
+        if (texture) and (model) then
+            table.insert(clothes, {texture, model, type})
+        end
+    end
+    
+    result = exports.api:saveDataApi(
+        username,
+        position,
+        rotation,
+        skin,
+        interior,
+        dimension,
+        team,
+        toJSON(weapons),
+        health,
+        armor,
+        money,
+        wantedlevel,
+        toJSON(clothes)
+    )
+
+    iprint("Save "..username.." data")
+end
+
+addEventHandler("onPlayerQuit", root, onPlayerQuit)
