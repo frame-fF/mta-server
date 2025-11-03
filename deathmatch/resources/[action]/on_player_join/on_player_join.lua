54e9bd46036ac2071f1840cfd065725cf0e81be6
fF <framef318@gmail.com>
2025-11-03 23:32:01 +0700

    Remove HUD hiding and cursor display from player join event

--- deathmatch/resources/[action]/on_player_join/on_player_join.lua
+++ deathmatch/resources/[action]/on_player_join/on_player_join.lua
@@ -1,7 +1,5 @@
 local function onPlayerJoin()
     outputChatBox("Welcome to the server! Please login or register.", source)
-    -- ซ่อน HUD และแสดงเคอร์เซอร์
-    triggerClientEvent(source, "showLoginPanel", source)
 end
 
 addEventHandler("onPlayerJoin", root, onPlayerJoin)
