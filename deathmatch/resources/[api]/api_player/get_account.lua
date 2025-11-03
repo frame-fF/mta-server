384f6ee904f6401293857fdeb91b08e162ef1993
fF <framef318@gmail.com>
2025-11-03 17:13:00 +0700

    Refactor get_account and get_player_account functions to improve player data handling and streamline login process

--- deathmatch/resources/[api]/api_player/get_account.lua
+++ deathmatch/resources/[api]/api_player/get_account.lua
@@ -12,8 +12,7 @@ function get_account(source, username, password)
     fetchRemote(url, sendOptions, function(data, info)
         if info.statusCode == 200 then
             outputChatBox('login success', source, 0, 255, 0)
-            -- Parse JSON data
-            local result = fromJSON(data)
+
             -- เช็คว่าเคยร์มีอยู่ในระบบหรือไม่
             local account = getAccount(username, password)
             if (account ~= false) then
@@ -25,47 +24,8 @@ function get_account(source, username, password)
                 logIn(source, new_account, password)
             end
 
-            local player_data = result.user.data
-
-            -- Set player name
-            setPlayerName(source, result.user.username)
-
-            -- Set player positiona
-            local x, y, z = player_data.position.x, player_data.position.y, player_data.position.z
-            -- Set player team
-            team = player_data.team and getTeamFromName(player_data.team)
-
-            spawnPlayer(
-                source,
-                x, y, z,
-                player_data.rotation,
-                player_data.skin,
-                player_data.interior,
-                player_data.dimension,
-                team
-            )
-
-            if player_data.weapons then
-                for weapon, ammo in pairs(player_data.weapons) do
-                    giveWeapon(source, tonumber(weapon), tonumber(ammo))
-                end
-            end
-
-            setElementHealth(source, player_data.health)
-            setPedArmor(source, player_data.armor)
-            setPlayerMoney(source, player_data.money)
-            setPlayerWantedLevel(source, player_data.wantedlevel)
-
-            clothes = unpack(player_data.clothes)
-            if player_data.clothes then
-                for _, cloth in ipairs(player_data.clothes) do
-                    addPedClothes(source, cloth[1], cloth[2], cloth[3])
-                end
-            end
-
-            fadeCamera(source, true)
-            setCameraTarget(source, source)
-
+            -- Parse JSON data
+            local result = fromJSON(data)
             setElementData(source, "results", result)
         else
             outputChatBox('Error: ' .. data, source, 255, 255, 0)
--- deathmatch/resources/[api]/api_player/get_player_account.lua
+++ deathmatch/resources/[api]/api_player/get_player_account.lua
@@ -9,6 +9,45 @@ function get_player_account(source)
     fetchRemote(url, sendOptions, function(data, info)
         if info.statusCode == 200 then
             local result = fromJSON(data)
+            local player_data = result.data
+            -- Set player name
+            setPlayerName(source, result.username)
+
+            -- Set player positiona
+            local x, y, z = player_data.position.x, player_data.position.y, player_data.position.z
+            -- Set player team
+            team = player_data.team and getTeamFromName(player_data.team)
+
+            spawnPlayer(
+                source,
+                x, y, z,
+                player_data.rotation,
+                player_data.skin,
+                player_data.interior,
+                player_data.dimension,
+                team
+            )
+
+            if player_data.weapons then
+                for weapon, ammo in pairs(player_data.weapons) do
+                    giveWeapon(source, tonumber(weapon), tonumber(ammo))
+                end
+            end
+
+            setElementHealth(source, player_data.health)
+            setPedArmor(source, player_data.armor)
+            setPlayerMoney(source, player_data.money)
+            setPlayerWantedLevel(source, player_data.wantedlevel)
+
+            clothes = unpack(player_data.clothes)
+            if player_data.clothes then
+                for _, cloth in ipairs(player_data.clothes) do
+                    addPedClothes(source, cloth[1], cloth[2], cloth[3])
+                end
+            end
+
+            fadeCamera(source, true)
+            setCameraTarget(source, source)
         else
             outputChatBox('Error: ' .. data, source, 255, 255, 0)
         end
