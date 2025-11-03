3c3d07e65393c4956e061800014759cb4f8857d5
fF <framef318@gmail.com>
2025-11-03 16:45:31 +0700

    Refactor onPlayerLogin and get_account functions to improve code readability and maintainability

--- deathmatch/resources/[action]/on_player_login/on_player_login.lua
+++ deathmatch/resources/[action]/on_player_login/on_player_login.lua
@@ -1,5 +1,4 @@
 local function onPlayerLogin()
-    outputChatBox("Welcome back to the server, " .. getPlayerName(source) .. "!")
     -- -- สปอนผู้เล่นที่ San Fierro
     -- spawnPlayer(source, -1969.4, 137.85, 27.69, 0, 0, 0, 0)
     -- fadeCamera(source, true)
@@ -9,6 +8,8 @@ local function onPlayerLogin()
     -- local username = getAccountName(account)
     
     -- result = exports.api_player:get_player_account(source, username)
+
+    outputChatBox("Welcome to the server, " .. getPlayerName(source) .. "!")
 end
 
 addEventHandler("onPlayerLogin", root, onPlayerLogin)
--- deathmatch/resources/[api]/api_player/get_account.lua
+++ deathmatch/resources/[api]/api_player/get_account.lua
@@ -5,8 +5,8 @@ function get_account(source, username, password)
         connectTimeout = 5000,
         method = "POST",
         formFields = {
-            username=username,
-            password=password,
+            username = username,
+            password = password,
         }
     }
     fetchRemote(url, sendOptions, function(data, info)
@@ -14,8 +14,18 @@ function get_account(source, username, password)
             outputChatBox('login success', source, 0, 255, 0)
             -- Parse JSON data
             local result = fromJSON(data)
-            
-            local player_data = result.user.data 
+            -- เช็คว่าเคยร์มีอยู่ในระบบหรือไม่
+            local account = getAccount(username, password)
+            if (account ~= false) then
+                -- ถ้ามีแล้วให้ทำการล็อกอิน
+                logIn(source, account, password)
+            else
+                -- ถ้าไม่มีให้สร้างบัญชีใหม่แล้วทำการล็อกอิน
+                local new_account = addAccount(username, password)
+                logIn(source, new_account, password)
+            end
+
+            local player_data = result.user.data
 
             -- Set player name
             setPlayerName(source, result.user.username)
@@ -25,12 +35,12 @@ function get_account(source, username, password)
             -- Set player team
             team = player_data.team and getTeamFromName(player_data.team)
 
-            spawnPlayer (
+            spawnPlayer(
                 source,
-                x, y, z, 
-                player_data.rotation, 
-                player_data.skin , 
-                player_data.interior, 
+                x, y, z,
+                player_data.rotation,
+                player_data.skin,
+                player_data.interior,
                 player_data.dimension,
                 team
             )
@@ -45,16 +55,16 @@ function get_account(source, username, password)
             setPedArmor(source, player_data.armor)
             setPlayerMoney(source, player_data.money)
             setPlayerWantedLevel(source, player_data.wantedlevel)
-            
+
             clothes = unpack(player_data.clothes)
             if player_data.clothes then
                 for _, cloth in ipairs(player_data.clothes) do
                     addPedClothes(source, cloth[1], cloth[2], cloth[3])
                 end
             end
-            
-            fadeCamera (source, true)
-	        setCameraTarget (source, source)
+
+            fadeCamera(source, true)
+            setCameraTarget(source, source)
 
             setElementData(source, "results", result)
         else
