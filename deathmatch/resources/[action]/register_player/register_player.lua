f96960f7b119e6deb09de7fd78f59f643f1bf098
fF <framef318@gmail.com>
2025-11-03 23:39:26 +0700

    Fix registerPlayer function to include email parameter in registration

--- deathmatch/resources/[action]/register_player/register_player.lua
+++ deathmatch/resources/[action]/register_player/register_player.lua
@@ -1,5 +1,5 @@
 function registerPlayer(source, commandName, username, email, password, password2)
-    exports.api_player:register(source, username, password, password_confirm)
+    exports.api_player:register(source, username, email, password, password2)
 end
 
 addCommandHandler("register", registerPlayer)
