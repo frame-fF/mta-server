bd84a8453e0f0cb4074961f2acca65cdf64ac384
fF <framef318@gmail.com>
2025-11-03 22:48:32 +0700

    Implement player registration functionality with email support

---
+++ deathmatch/resources/[api]/api_player/register.lua
@@ -0,0 +1,21 @@
+function register(source, username, email, password, password2)
+    local url = "http://127.0.0.1:8000" .. '/api/player/register/'
+    local sendOptions = {
+        connectionAttempts = 3,
+        connectTimeout = 5000,
+        method = "POST",
+        formFields = {
+            username = username,
+            email = email,
+            password = password,
+            password2 = password2
+        }
+    }
+    fetchRemote(url, sendOptions, function(data, info)
+        if info.statusCode == 201 then
+            outputChatBox('Registration successful! You can now log in.', source, 0, 255, 0)
+        else
+            outputChatBox('Error: ' .. data, source, 255, 255, 0)
+        end
+    end)
+end
