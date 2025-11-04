3c95c168f4463f1d0fc0b33182d92847e94f950d
fF <framef318@gmail.com>
2025-11-04 18:39:47 +0700

    Add gameplay resources and implement pickup functionality; update base URL in API settings

---
+++ deathmatch/resources/[system]/get_pos/get_pos.lua
@@ -0,0 +1,8 @@
+function getPosition(source)
+    x, y, z = getElementPosition(source)
+    rx, ry, rz = getElementRotation(source)
+    outputChatBox("Posicion: " .. x .. ", " .. y .. ", " .. z, source, 255, 255, 0)
+    outputChatBox("Rotacion: " .. rx .. ", " .. ry .. ", " .. rz, source, 255, 255, 0)
+end
+
+addCommandHandler("get_pos", getPosition)
