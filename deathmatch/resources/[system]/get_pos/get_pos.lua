68986792aa47e7067445429d5a0d011694a0c8ad
fF <framef318@gmail.com>
2025-11-05 10:14:37 +0700

    Add interior and dimension information to getPosition command output

--- deathmatch/resources/[system]/get_pos/get_pos.lua
+++ deathmatch/resources/[system]/get_pos/get_pos.lua
@@ -1,8 +1,13 @@
 function getPosition(source)
     x, y, z = getElementPosition(source)
     rx, ry, rz = getElementRotation(source)
+    local interior = getElementInterior(source)
+    local dimension = getElementDimension(source)
     outputChatBox("Posicion: " .. x .. ", " .. y .. ", " .. z, source, 255, 255, 0)
     outputChatBox("Rotacion: " .. rx .. ", " .. ry .. ", " .. rz, source, 255, 255, 0)
+    outputChatBox("Interior: " .. interior, source, 255, 255, 0)
+    outputChatBox("Dimension: " .. dimension, source, 255, 255, 0)
 end
 
 addCommandHandler("get_pos", getPosition)
+
