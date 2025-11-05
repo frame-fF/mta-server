7e6c3e3382a69c0defb6211e5ad2fa9a62aff781
fF <framef318@gmail.com>
2025-11-05 10:29:55 +0700

    Refactor getPosition function to remove interior and dimension output; add interior warp event handler for detailed interior information

--- deathmatch/resources/[system]/get_pos/get_pos.lua
+++ deathmatch/resources/[system]/get_pos/get_pos.lua
@@ -1,13 +1,19 @@
 function getPosition(source)
     x, y, z = getElementPosition(source)
     rx, ry, rz = getElementRotation(source)
-    local interior = getElementInterior(source)
-    local dimension = getElementDimension(source)
     outputChatBox("Posicion: " .. x .. ", " .. y .. ", " .. z, source, 255, 255, 0)
     outputChatBox("Rotacion: " .. rx .. ", " .. ry .. ", " .. rz, source, 255, 255, 0)
-    outputChatBox("Interior: " .. interior, source, 255, 255, 0)
-    outputChatBox("Dimension: " .. dimension, source, 255, 255, 0)
 end
 
 addCommandHandler("get_pos", getPosition)
 
+addEventHandler("onPlayerInteriorWarped", root,
+    function(warpedInterior)
+        local interiorName = exports.interiors:getInteriorName(warpedInterior)
+        local interior = getElementInterior(source)
+        local dimension = getElementDimension(source)
+        outputChatBox("Has entrado en el interior: " .. interiorName, source, 0, 255, 0)
+        outputChatBox("Interior ID: " .. interior, source, 255, 255, 0)
+        outputChatBox("Dimension ID: " .. dimension, source, 255, 255, 0)
+    end
+)
