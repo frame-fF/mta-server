83a9df64e740c1593e6b5b10e5c19d6efdb32aab
fF <framef318@gmail.com>
2025-11-06 14:11:20 +0700

    Update attachment offsets for ak47 and sni to improve positioning and alignment

--- deathmatch/resources/[system]/attach/client.lua
+++ deathmatch/resources/[system]/attach/client.lua
@@ -1,8 +1,6 @@
 local ak47 = createObject(355, 0, 0, 0)
 local sni = createObject(358, 0, 0, 0)
 
-exports.pAttach:attach(ak47, getLocalPlayer(), "spine", 0, -0.15, 0, 0, 0, 0)
-exports.pAttach:attach(sni, getLocalPlayer(), "spine",  0, -0.15, 0, 0, 0, 0)
-
-exports.pAttach:setPositionOffset(ak47, 0, -0.15, 0)
-exports.pAttach:setPositionOffset(sni, 0, -0.15, 0)
+exports.pAttach:attach(ak47, getLocalPlayer(), "spine", 0.25, -0.15, 0.2, -110, -180, 0)
+exports.pAttach:attach(sni, getLocalPlayer(), "spine",  0.25, -0.15, -0.2, -70, -180, 0)
+    
