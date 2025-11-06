41048e62c9ed2f48bf31889affdfae6cfc50461c
fF <framef318@gmail.com>
2025-11-06 16:25:55 +0700

    Add settings resource and base URL function for player login

--- deathmatch/resources/[gameplay]/login/server.lua
+++ deathmatch/resources/[gameplay]/login/server.lua
@@ -1,5 +1,18 @@
 function loginPlayer(source, command, username, password)
-    outputChatBox("Login command received. Username: " .. tostring(username) .. ", Password: " .. tostring(password), source)
+    login(source, username, password)
 end
 
 addCommandHandler("log-in", loginPlayer) 
+
+function login(source, username, password)
+    local url = exports.settings:baseUrl() .. '/api/player/login/'
+    local sendOptions = {
+        connectionAttempts = 3,
+        connectTimeout = 5000,
+        method = "POST",
+        formFields = {
+            username = username,
+            password = password,
+        }
+    }
+end
---
+++ deathmatch/resources/settings/server.lua
@@ -0,0 +1,3 @@
+function baseUrl()
+    return "http://127.0.0.1:8000"
+end
