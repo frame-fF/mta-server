dfcb83c9ae64cd3a1b3186a0ddee31effa74198a
fF <framef318@gmail.com>
2025-11-06 14:25:28 +0700

    Update attachment points for ak47 and sni to improve positioning and alignment

--- deathmatch/resources/[system]/attach/client.lua
+++ deathmatch/resources/[system]/attach/client.lua
@@ -1,6 +1,6 @@
 local ak47 = createObject(355, 0, 0, 0)
 local sni = createObject(358, 0, 0, 0)
 
-exports.pAttach:attach(ak47, getLocalPlayer(), "spine", 0.25, -0.15, 0.2, -110, -180, 0)
-exports.pAttach:attach(sni, getLocalPlayer(), "spine",  0.25, -0.15, -0.2, -70, -180, 0)
+exports.pAttach:attach(sni, getLocalPlayer(), "backpack", 0.25, -0.12, 0.24, -90, -180, 0)
+exports.pAttach:attach(ak47, getLocalPlayer(), "backpack",  0.25, -0.12, -0.2, -90, -180, 0)
     
