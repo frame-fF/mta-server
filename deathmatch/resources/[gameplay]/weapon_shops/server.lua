6670f0ef91e2ff74cac59a08d32a0db0d5d02b88
fF <framef318@gmail.com>
2025-11-08 00:04:45 +0700

    Add initial setup for weapon shops with blip, marker, and NPC

---
+++ deathmatch/resources/[gameplay]/weapon_shops/server.lua
@@ -0,0 +1,7 @@
+local blip_1 = createBlip(-2625.85, 208.345, 3.98935, 6, 2, 255, 0, 0, 255, 0, 300)
+local mark_1 = createMarker(295.626953125, -37.5712890625, 1000.5, "cylinder", 2, 255, 255, 0, 255)
+local npc_1 = createPed(179, 295.669921875, -40.45703125, 1001.515625)
+setElementInterior(mark_1, 1)
+setElementDimension(mark_1, 1)
+setElementInterior(npc_1, 1)
+setElementDimension(npc_1, 1)
