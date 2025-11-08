6140926cf269da1cc692e12788d86cf5161991aa
fF <framef318@gmail.com>
2025-11-08 20:03:12 +0700

    Add weapon data structure with various weapon types and descriptions

---
+++ deathmatch/resources/[gameplay]/inventories/data.lua
@@ -0,0 +1,30 @@
+DATA_WEAPON = {
+    -- Handguns 
+    [22] = { name = "Colt 45", description="" }, -- Colt 45
+    [23] = { name = "Silenced", description="" }, -- Silenced
+    [24] = { name = "Desert Eagle", description="" },  -- Desert Eagle
+    -- Shotguns 
+    [25] = { name = "Shotgun", description="" },-- Shotgun
+    [26] = { name = "Sawed-off", description="" },-- Sawed-off
+    [27] = { name = "Combat Shotgun", description="" },  -- Combat Shotgun
+    -- Sub-Machine Guns
+    [28] = { name = "Uzi", description="" },-- Uzi
+    [29] = { name = "MP5", description="" },-- MP5
+    [32] = { name = "Tec-9", description="" },  -- Tec-9
+    -- Assault Rifles 
+    [30] = { name = "Ak47", description="" },-- Ak47
+    [31] = { name = "M4", description="" },-- M4
+    -- Rifles
+    [33] = { name = "Rifle", description="" },-- Rifle
+    [34] = { name = "Sniper Rifle", description="" },-- Sniper Rifle
+    --Heavy Weapons
+    [35] = { name = "Rocket Launcher", description="" },-- Rocket Launcher
+    [36] = { name = "Rocket Launcher HS", description="" }, -- Rocket Launcher HS
+    [37] = { name = "Flame", description="" },-- Flame
+    [38] = { name = "Minigun", description="" },-- Minigun
+    --Projectiles 
+    [16] = { name = "Grenade", description="" },-- Grenade
+    [17] = { name = "Tear Gas", description="" },-- Tear Gas
+    [18] = { name = "Molotov", description="" },-- Molotov
+    [39] = { name = "Satchel", description="" },-- Satchel
+}
