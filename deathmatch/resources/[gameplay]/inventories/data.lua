6617a10833dceb6645bc98bd648eeffbc4db43d5
fF <framef318@gmail.com>
2025-11-08 20:49:56 +0700

    Refactor code structure for improved readability and maintainability

--- deathmatch/resources/[gameplay]/inventories/data.lua
+++ deathmatch/resources/[gameplay]/inventories/data.lua
@@ -1,30 +1,55 @@
 DATA_WEAPON = {
     -- Handguns 
-    [22] = { name = "Colt 45", description="" }, -- Colt 45
-    [23] = { name = "Silenced", description="" }, -- Silenced
-    [24] = { name = "Desert Eagle", description="" },  -- Desert Eagle
+    [2] = {
+        [22] = { name = "Colt 45", description="" }, -- Colt 45
+        [23] = { name = "Silenced", description="" }, -- Silenced
+        [50] = { name = "9mm Ammo for Colt 45", description="" }, -- 9mm Ammo for Colt 45
+        [24] = { name = "Desert Eagle", description="" },  -- Desert Eagle
+        [51] = { name = "Desert Eagle Ammo", description="" },  -- Desert Eagle Ammo
+    },
     -- Shotguns 
-    [25] = { name = "Shotgun", description="" },-- Shotgun
-    [26] = { name = "Sawed-off", description="" },-- Sawed-off
-    [27] = { name = "Combat Shotgun", description="" },  -- Combat Shotgun
+    [3] = {
+        [25] = { name = "Shotgun", description="" },-- Shotgun
+        [26] = { name = "Sawed-off", description="" },-- Sawed-off
+        [27] = { name = "Combat Shotgun", description="" },  -- Combat Shotgun
+        [52] = { name = "Shotgun Ammo", description="" },  -- Shotgun Ammo
+    },
     -- Sub-Machine Guns
-    [28] = { name = "Uzi", description="" },-- Uzi
-    [29] = { name = "MP5", description="" },-- MP5
-    [32] = { name = "Tec-9", description="" },  -- Tec-9
+    [4] = {
+        [28] = { name = "Uzi", description="" },-- Uzi
+        [29] = { name = "MP5", description="" },-- MP5
+        [32] = { name = "Tec-9", description="" },  -- Tec-9
+        [53] = { name = "SMG Ammo", description="" },  -- SMG Ammo
+    },
     -- Assault Rifles 
-    [30] = { name = "Ak47", description="" },-- Ak47
-    [31] = { name = "M4", description="" },-- M4
+    [5] = {
+        [30] = { name = "Ak47", description="" },-- Ak47
+        [31] = { name = "M4", description="" },-- M4
+        [54] = { name = "Assault Rifle Ammo", description="" },  -- Assault Rifle Ammo
+    },
     -- Rifles
-    [33] = { name = "Rifle", description="" },-- Rifle
-    [34] = { name = "Sniper Rifle", description="" },-- Sniper Rifle
+    [6] = {
+        [33] = { name = "Rifle", description="" },-- Rifle
+        [34] = { name = "Sniper Rifle", description="" },-- Sniper Rifle
+        [55] = { name = "Rifle Ammo", description="" },  -- Rifle Ammo
+        [56] = { name = "Sniper Ammo", description="" },  -- Sniper Ammo
+    },
     --Heavy Weapons
-    [35] = { name = "Rocket Launcher", description="" },-- Rocket Launcher
-    [36] = { name = "Rocket Launcher HS", description="" }, -- Rocket Launcher HS
-    [37] = { name = "Flame", description="" },-- Flame
-    [38] = { name = "Minigun", description="" },-- Minigun
+    [7] = {
+        [35] = { name = "Rocket Launcher", description="" },-- Rocket Launcher
+        [36] = { name = "Rocket Launcher HS", description="" }, -- Rocket Launcher HS
+        [37] = { name = "Flame", description="" },-- Flame
+        [38] = { name = "Minigun", description="" },-- Minigun
+        [57] = { name = "Rocket Ammo", description="" },  -- Rocket Ammo
+        [58] = { name = "HS Rocket Ammo", description="" },  -- HS Rocket Ammo
+        [59] = { name = "Flame Ammo", description="" },  -- Flame Ammo
+        [60] = { name = "Minigun Ammo", description="" },  -- Minigun Ammo
+    },
     --Projectiles 
-    [16] = { name = "Grenade", description="" },-- Grenade
-    [17] = { name = "Tear Gas", description="" },-- Tear Gas
-    [18] = { name = "Molotov", description="" },-- Molotov
-    [39] = { name = "Satchel", description="" },-- Satchel
+    [8] = {
+        [16] = { name = "Grenade", description="" },-- Grenade
+        [17] = { name = "Tear Gas", description="" },-- Tear Gas
+        [18] = { name = "Molotov", description="" },-- Molotov
+        [39] = { name = "Satchel", description="" },-- Satchel
+    },
 }
