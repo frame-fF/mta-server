a54eb50e34b6c11f7fb696a5c11e68e38b3ed8a7
fF <framef318@gmail.com>
2025-11-09 11:18:46 +0700

    Add weapon data configuration to inventory

---
+++ deathmatch/resources/[gameplay]/inventory/data.lua
@@ -0,0 +1,24 @@
+DATA_WEAPON = {
+    [22]={slot=2, ammo_id=50, name="Colt 45"},
+    [23]={slot=2, ammo_id=50, name="Silenced Pistol"},
+    [24]={slot=2, ammo_id=51, name="Desert Eagle"},
+
+    [25]={slot=3, ammo_id=52, name="Shotgun"},
+    [26]={slot=3, ammo_id=52, name="Sawn-off Shotgun"},
+    [27]={slot=3, ammo_id=52, name="Combat Shotgun"},
+
+    [28]={slot=4, ammo_id=53, name="Uzi"},
+    [29]={slot=4, ammo_id=53, name="MP5"},
+    [32]={slot=4, ammo_id=53, name="Tec-9"},
+
+    [30]={slot=4, ammo_id=54, name="AK-47"},
+    [31]={slot=4, ammo_id=54, name="M4"},
+
+    [33]={slot=5, ammo_id=55, name="Rifle"},
+    [34]={slot=5, ammo_id=56, name="Sniper Rifle"},
+
+    [35]={slot=6, ammo_id=57, name="Rocket Launcher"},
+    [36]={slot=6, ammo_id=58, name="Heat-Seeking Rocket Launcher"},
+    [37]={slot=7, ammo_id=59, name="Flamethrower"},
+    [38]={slot=7, ammo_id=60, name="Minigun"},
+}
