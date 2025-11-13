07e87cbc2157a31c04d9243daac4b7803a254c13
fF <framef318@gmail.com>
2025-11-13 12:21:36 +0700

    Comment out backpack functionality and player join event handler in server.lua

---
+++ deathmatch/resources/[gameplay]/skins/server.lua
@@ -0,0 +1,8 @@
+-- local function addBackpack(player)
+--     local backpack = createObject(371, 0, 0, 0)
+--     exports.pAttach:attach(backpack, player, "backpack", 0.1, -0.15, 0, 0, 90, 0)
+-- end
+
+-- addEventHandler("onPlayerJoin", root, function()
+--     addBackpack(source)
+-- end)
