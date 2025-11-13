81ed6895fe64204e025dc29db1efc813abc7192f
fF <framef318@gmail.com>
2025-11-13 13:33:33 +0700

    Refactor backpack attachment logic and update event handler to onPlayerResourceStart

--- deathmatch/resources/[gameplay]/skins/server.lua
+++ deathmatch/resources/[gameplay]/skins/server.lua
@@ -1,9 +1,10 @@
 local function addBackpack(player)
-    local x,y,z = getElementPosition(player)
-    local backpack = createObject(371, x,y,z)
-    exports.pAttach:attach(backpack, player, "backpack", 0,-0.225,0.05,90,0,0)
+    local x, y, z = getElementPosition(player)
+    
+    local backpack = createObject(371, x, y, z)
+    exports.pAttach:attach(backpack, player, "backpack", 0, -0.16, 0.05, 270, 0, 180)
 end
 
-addEventHandler("onPlayerJoin", root, function()
+addEventHandler("onPlayerResourceStart", root, function()
     addBackpack(source)
 end)
