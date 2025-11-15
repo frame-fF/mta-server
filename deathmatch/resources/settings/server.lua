ee4f84468a65362ef7de8388e6eb326cca89ee72
fF <framef318@gmail.com>
2025-11-15 18:58:06 +0700

    Fix baseUrl function to ensure consistent server address usage in return value

--- deathmatch/resources/settings/server.lua
+++ deathmatch/resources/settings/server.lua
@@ -1,5 +1,5 @@
 function baseUrl()
     -- http://mta-backend:8888 
-    -- http://127.0.0.1:8000 
-    return "http://mta-backend:8888"
+    -- http://127.0.0.1:8000
+    return "http://127.0.0.1:8000"
 end
