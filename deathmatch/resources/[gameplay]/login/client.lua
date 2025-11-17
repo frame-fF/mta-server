bcf4a24e2febf7d25541f9ce27ea0099076a8ba3
fF <framef318@gmail.com>
2025-11-17 11:26:55 +0700

    Implement initial structure for login GUI in client.lua

--- deathmatch/resources/[gameplay]/login/client.lua
+++ deathmatch/resources/[gameplay]/login/client.lua
@@ -1,1 +1,15 @@
 -- login gui
+local function createLoginGUI()
+
+end
+
+local function hideLoginGUI()
+
+end
+
+addEventHandler("onClientResourceStart", resourceRoot,
+    function()
+        -- สร้าง GUI สำหรับล็อกอิน
+        createLoginGUI()
+    end
+)
