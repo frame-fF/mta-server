bfa724b92950ff6c3196ae465133422dd3f5c1dc
fF <framef318@gmail.com>
2025-11-24 16:19:36 +0700

    direk

---
+++ deathmatch/resources/[addons]/direk/client.lua
@@ -0,0 +1,42 @@
+dff = engineLoadDFF ('gay_lamppost.dff', 3853)
+engineReplaceModel (dff, 3853)
+
+dff = engineLoadDFF ('gay_telgrphpole.dff', 3854)
+engineReplaceModel (dff, 3854)
+
+dff = engineLoadDFF ('lampost_coast.dff', 1223)
+engineReplaceModel (dff, 1223)
+
+dff = engineLoadDFF ('lamppost1.dff', 1297)
+engineReplaceModel (dff, 1297)
+
+dff = engineLoadDFF ('lamppost2.dff', 1290)
+engineReplaceModel (dff, 1290)
+
+dff = engineLoadDFF ('lamppost3.dff', 1226)
+engineReplaceModel (dff, 1226)
+
+dff = engineLoadDFF ('mlamppost.dff', 1294)
+engineReplaceModel (dff, 1294)
+
+dff = engineLoadDFF ('telgrphpole02.dff', 1308)
+engineReplaceModel (dff, 1308)
+
+dff = engineLoadDFF ('telgrphpoleall.dff', 1307)
+engineReplaceModel (dff, 1307)
+
+dff = engineLoadDFF ('vegaslampost2.dff', 3463)
+engineReplaceModel (dff, 3463)
+
+dff = engineLoadDFF ('vgntelepole1.dff', 3459)
+engineReplaceModel (dff, 3459)
+
+
+
+-- Sitemiz : https://sparrow-mta.blogspot.com/
+
+-- Facebook : https://facebook.com/sparrowgta/
+-- İnstagram : https://instagram.com/sparrowmta/
+-- YouTube : https://youtube.com/c/SparroWMTA/
+
+-- Discord : https://discord.gg/DzgEcvy
