e3b60ff83ee286877051a5799bbb4c4b63e6aed7
fF <framef318@gmail.com>
2025-11-25 21:15:27 +0700

    yatyere-c

---
+++ deathmatch/resources/[anims]/yatyere-c/client.lua
@@ -0,0 +1,13 @@
+﻿local customBlockName = "Rifle"
+
+-- load the IFP file
+local IFP = engineLoadIFP( "rifle.ifp", customBlockName )
+
+-- let us know if IFP failed to load
+if not IFP then
+    outputChatBox( "Failed to load 'rifle.ifp'" )
+end
+
+-- replace the crouch animation
+engineReplaceAnimation( localPlayer, "Rifle", "RIFLE_crouchfire", customBlockName, "RIFLE_crouchfire" )
+
